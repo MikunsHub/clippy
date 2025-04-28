a16501cf1e6ab395b3e6bcdf6c2e6ac819c700fd
MikunsHub <ogunjuay@gmail.com>
2025-04-28 21:17:10 +0100

    Initial project setup(wails,vite and tailwindcss)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+import './index.css'
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500"> Tailwind CSS</h1>
+    </>
+  )
+}
+
+export default App
